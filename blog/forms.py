87470fc7f22ce4a31885aa0b91cc96fdf418ae49
dhowai <dhowai.dev@gmail.com>
2021-11-21 21:03:35 +0000

    Add feature to edit a post made

--- blog/forms.py
+++ blog/forms.py
@@ -12,3 +12,15 @@ class AddPostForm(forms.ModelForm):
             'author': forms.Select(attrs={'class': 'form-control'}),
             'content_image': forms.FileInput(attrs={'class': 'form-control'}),
         }
+
+
+class EditPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'content_image')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'content_image': forms.FileInput(attrs={'class': 'form-control'}),
+        }
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
-from .forms import AddPostForm
+from .forms import AddPostForm, EditPostForm
 
 
 class HomeView(ListView):
@@ -20,3 +20,9 @@ class AddPostView(CreateView):
     model = Post
     form_class = AddPostForm
     template_name = 'add_post.html'
+
+
+class EditPostView(UpdateView):
+    model = Post
+    form_class = EditPostForm
+    template_name = 'edit_post.html'
