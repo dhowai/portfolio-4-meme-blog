0cf2b6f1d82c28375b68d1ebb7e6285995e99c78
dhowai <dhowai.dev@gmail.com>
2021-11-21 21:41:37 +0000

    Add feature to delete a post made

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView, PostDetailView, AddPostView, EditPostView
+from .views import HomeView, PostDetailView, AddPostView, EditPostView, DeletePostView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
     path('add_post/', AddPostView.as_view(), name='add_post'),
     path('post/edit/<int:pk>', EditPostView.as_view(), name='edit_post'),
+    path('post/<int:pk>/delete', DeletePostView.as_view(), name='delete_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import AddPostForm, EditPostForm
+from django.urls import reverse_lazy
 
 
 class HomeView(ListView):
@@ -26,3 +27,9 @@ class EditPostView(UpdateView):
     model = Post
     form_class = EditPostForm
     template_name = 'edit_post.html'
+
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
