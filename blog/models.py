3b91b07e35c60d488c16d86603a6f57bd30eea4e
dhowai <dhowai.dev@gmail.com>
2021-12-03 03:29:23 +0000

    Add comment model and structure to post_detail.html

--- blog/models.py
+++ blog/models.py
@@ -34,3 +34,17 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return reverse('home')
+
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=100)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
