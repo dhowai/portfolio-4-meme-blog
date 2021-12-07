abaaa8844f4e91e73ca6a25d5378cf5315fc8868
dhowai <dhowai.dev@gmail.com>
2021-12-07 17:32:41 +0000

    Add ability for user to edit their profile

--- members/urls.py
+++ members/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView, UserEditProfileView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
+    path('edit_profile/', UserEditProfileView.as_view(), name='edit_profile'),
 ]
