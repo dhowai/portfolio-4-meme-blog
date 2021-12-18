0b027a8d7460578e2d6db24c5172365e206b4234
dhowai <dhowai.dev@gmail.com>
2021-12-18 18:25:52 +0000

    Add alert messages and add js folder and script to static

--- members/views.py
+++ members/views.py
@@ -2,28 +2,32 @@ from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
 from .forms import RegisterForm, EditUserSettingsForm, PasswordChangingForm, ProfilePageForm, EditProfilePageForm
 from memeblog.models import UserProfile
 
 
-class UserRegisterView(generic.CreateView):
+class UserRegisterView(SuccessMessageMixin, generic.CreateView):
     form_class = RegisterForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+    success_message = "Registered Succesfully"
 
 
-class UserEditSettingsView(generic.UpdateView):
+class UserEditSettingsView(SuccessMessageMixin, generic.UpdateView):
     form_class = EditUserSettingsForm
     template_name = 'registration/edit_user_settings.html'
     success_url = reverse_lazy('home')
+    success_message = "User Settings Updated"
 
     def get_object(self):
         return self.request.user
 
 
-class PasswordsChangeView(PasswordChangeView):
+class PasswordsChangeView(SuccessMessageMixin, PasswordChangeView):
     form_class = PasswordChangingForm
     success_url = reverse_lazy('home')
+    success_message = "Password Succesfully Updated"
 
 
 class ProfilePageView(generic.DetailView):
@@ -39,18 +43,20 @@ class ProfilePageView(generic.DetailView):
         return context
 
 
-class EditProfilePageView(generic.UpdateView):
+class EditProfilePageView(SuccessMessageMixin, generic.UpdateView):
     model = UserProfile
     form_class = EditProfilePageForm
     template_name = 'registration/edit_profile_page.html'
     success_url = reverse_lazy('home')
+    success_message = "Profile Succesfully Updated"
 
 
-class CreateProfilePageView(generic.CreateView):
+class CreateProfilePageView(SuccessMessageMixin, generic.CreateView):
     model = UserProfile
     form_class = ProfilePageForm
     template_name = 'registration/create_profile_page.html'
-    
+    success_message = "Profile Succesfully Created, You can now make Posts!"
+
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
